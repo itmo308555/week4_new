a5b86c5e1efcdb064b6f66ca7aac047f98b073c8
itmo308555 <92982321+itmo308555@users.noreply.github.com>
2021-10-30 15:51:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,13 +18,8 @@ const server = http.createServer(async(req,res)=>{
           })
           res.write(JSON.stringify({
             "message":"itmo308555",
-<<<<<<< HEAD
-            "x-result":"headers",
-            "x-body":"data"
-=======
             "x-result":"baovn",
             "x-body":"abc"
->>>>>>> 41e4df2f04adfb47249bb10c5b5954cffe99d1e0
             }
             ))
       }
